cac161aa2d114bddb4234dc4a4d70b2e438c9ac8
GHOST-Aram <owinoojala@gmail.com>
2023-07-05 14:39:17 +0300

    chore: Decouple class declaration from usage
          Class Emmitter

--- eventEmitter.js
+++ eventEmitter.js
@@ -1,70 +1,49 @@
 import { EventEmitter } from 'node:events'
-const eventEmitter = new EventEmitter()
 
 export class Emitter {
-    addListener = (listener, handler) =>{
-        new EventEmitter().addListener(listener, handler)
+    #eventEmitter = new EventEmitter()
+
+    addListener = (event, handler) =>{
+        this.#eventEmitter.addListener(event, handler)
     }
 
-    prependListener = (listener, handler)=>{
-        new EventEmitter().prependListener(listener, handler)
+    emit = (event, param) =>{
+        this.#eventEmitter.emit(event, param)
     }
 
-    emit = (listener, param) =>{
-        eventEmitter.emit(listener, param)
+    getEventNames = () =>{
+        return this.#eventEmitter.eventNames()
+    }
+
+    getMaxListeners = () =>{
+        return this.#eventEmitter.getMaxListeners()
+    }
+
+    getListeners = (event) =>{
+        return this.#eventEmitter.listeners(event)
+    }
+
+    listenerCount = (event) =>{
+        return this.#eventEmitter.listenerCount(event)
+    }
+
+    on = (event, handler) => {
+        this.#eventEmitter.on(event, handler)
+    }
+
+    once = (event, handler) =>{
+        this.#eventEmitter.once(event, handler)
+    }
+
+    prependListener = (event, handler)=>{
+        this.#eventEmitter.prependListener(event, handler)
+    }
+    
+    removeListener = (event, handler) =>{
+        this.#eventEmitter.removeListener(event, handler)
     }
 
     setMaxListeners = (max) =>{
-        eventEmitter.setMaxListeners(max)
-    }
-    on = (listener, handler) => {
-        eventEmitter.on(listener, handler)
+        this.#eventEmitter.setMaxListeners(max)
     }
 }
-
-
-const kill = (name) => console.log(`${name} is dead.`)
-eventEmitter.prependListener('violence  ', (someLaw) => {
-    console.log('Registered: ', someLaw)
-})
-eventEmitter.addListener('start', (name) => kill(name))
-
-eventEmitter.on('start', (name, age) =>{
-    console.log(
-        'Event Started', name, age , ' years'
-    )
-})
-eventEmitter.on('start', (name) =>{
-    console.log(
-        'Event Started', name, ' years'
-    )
-})
-
-eventEmitter.addListener('stop', (action, time) => {
-    console.log(`Stop ${action} at ${time}`)
-})
-
-eventEmitter.once('silence', (action) =>{
-    console.log('currently: ' , action)
-})
-
-eventEmitter.emit(
-    'violence', ' Do not steal'
-)
-
-eventEmitter.setMaxListeners(20)
-// eventEmitter.removeAllListeners('start')
-eventEmitter.removeListener('start', (name) => kill(name))
-eventEmitter.emit('silence', 'Waking')
-eventEmitter.emit('start', 'Erick', 33)
-eventEmitter.emit('start', 'Erick', 33)
-eventEmitter.emit('start', 'Erick', 33)
-eventEmitter.emit('stop', 'running', '1400hrs')
-eventEmitter.emit('silence', 'Sleeping')
-eventEmitter.emit('start', 'joy')
-
-console.log(eventEmitter.eventNames())
-console.log(eventEmitter.getMaxListeners())
-console.log(eventEmitter.listenerCount('start'))
-console.log(eventEmitter.listeners('stop'))
-// eventEmitter.off('start')
